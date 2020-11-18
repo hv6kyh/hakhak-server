6ce872406b923964165ad580d6eb549bb376af30
김영호 <youngho@gim-yeonghoui-MacBookPro.local>
2020-11-18 22:03:55 +0900

    jwt 검증 에러시 명시적 에러 반환

--- src/shared/auth/jwt.guard.ts
+++ src/shared/auth/jwt.guard.ts
@@ -1,11 +1,21 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { ApolloError } from 'apollo-server-express';
+import { Exception } from '../constant';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   getRequest(context: ExecutionContext) {
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
+  }
+
+  handleRequest(err, user) {
+    if (err || !user) {
+      throw err ||
+        new ApolloError('jwt verification failed', Exception.UNAUTHORIZED);
+    }
+    return user;
   }
 }
